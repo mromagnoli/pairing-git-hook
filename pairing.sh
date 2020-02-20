3e3b44b0f1cfa6bde147a8b05e80b572bb2dd7a3
Marcelo Romagnoli <marce.romagnoli@gmail.com>
2020-02-20 21:02:11 +0100

    First attempt on working version

--- pairing.sh
+++ pairing.sh
@@ -1,26 +1,50 @@
 #!/bin/bash
 
+COMMIT_MSG_FILE=$1
+COMMIT_SOURCE=$2
+SHA1=$3
+
 check_pair() {
     pair_alias=$1
+    local result="Fail"
+    while IFS=, read -r user_alias name email
+        do
+            if [[ "$user_alias" == "$pair_alias" ]]; then
+                local result="Co-Authored-By: $name <$email>"
+                break 1
+                break 2
+            fi
+        done < .pairs
 
-    while IFS= read -r line
-    do
-    echo "$line"
-    done < .pairs
+    echo "$result"
+}
+
+write_commit_message() {
+    message=$1
+
+    if [ "$message" = "Fail" ]; then
+        echo "Not valid pair"
+        exit 1
+    fi
+
+    @PERL_PATH@ -i.bak -ne 'print $message' "$COMMIT_MSG_FILE"
 }
 
 
 echo "Did you pair this with some team folk?"
+exec < /dev/tty
 
-read -p "You pair alias: " pair
+read -p "Your pair alias: " pair
 
 case $pair in
 
     no)
         exit 0
     ;;
-    yes)
-        check_pair "chelo"
+    *)
+        res="$(check_pair $pair)"
+        # echo $res
+        write_commit_message $res
         exit 0
     ;;
 esac
